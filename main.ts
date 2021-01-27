8a1b4aa95b1e88c1ea42a93c1de0b4be021543fc
Jexanti <edwardmwild@gmail.com>
2021-01-27 15:14:14 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(9 / 5 * input.temperature() + 32)
 })
